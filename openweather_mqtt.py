1a7b0ee5749d11e5cb6fc3f93dafed8568ff9f4c
Henry Klingberg <henry.klingberg@itemis.de>
2024-11-18 08:31:42 +0100

    make weather available for zetten

--- openweather_mqtt.py
+++ openweather_mqtt.py
@@ -13,28 +13,44 @@ load_dotenv()
 
 # Config from environment (see Dockerfile)
 OPENWEATHER_APP_ID = os.getenv('OPENWEATHER_APP_ID', 'YOUR_OPENWEATHER_APP_ID')
-OPENWEATHER_CITY_ID = os.getenv('OPENWEATHER_CITY_ID', 'YOUR_OPENWEATHER_CITY_ID')
+OPENWEATHER_CITY_ID_1 = os.getenv('OPENWEATHER_CITY_ID_1', 'YOUR_OPENWEATHER_CITY_ID_1')
+OPENWEATHER_CITY_ID_2 = os.getenv('OPENWEATHER_CITY_ID_2', 'YOUR_OPENWEATHER_CITY_ID_2')
 
-MQTT_SERVICE_HOST = os.getenv('MQTT_SERVICE_HOST', 'mosquitto.local')
-MQTT_SERVICE_PORT = int(os.getenv('MQTT_SERVICE_PORT', 1883))
-MQTT_SERVICE_TOPIC = os.getenv('MQTT_SERVICE_TOPIC', 'openweather')
-MQTT_CLIENT_ID = os.getenv('HOSTNAME', 'openweather-mqtt-service')
-MQTT_SERVICE_USER = os.getenv('MQTT_SERVICE_USER')
-MQTT_SERVICE_PW = os.getenv('MQTT_SERVICE_PW')
+MQTT_SERVICE_HOST_1 = os.getenv('MQTT_SERVICE_HOST_1', 'mosquitto.local')
+MQTT_SERVICE_HOST_2 = os.getenv('MQTT_SERVICE_HOST_2', 'mosquitto.local')
+MQTT_SERVICE_PORT_1 = int(os.getenv('MQTT_SERVICE_PORT_1', 1883))
+MQTT_SERVICE_PORT_2 = int(os.getenv('MQTT_SERVICE_PORT_2', 1883))
+MQTT_SERVICE_TOPIC_1 = os.getenv('MQTT_SERVICE_TOPIC_1', 'openweather')
+MQTT_SERVICE_TOPIC_2 = os.getenv('MQTT_SERVICE_TOPIC_2', 'openweather')
+MQTT_CLIENT_ID_1 = os.getenv('HOSTNAME_1', 'openweather-mqtt-service')
+MQTT_CLIENT_ID_2 = os.getenv('HOSTNAME_2', 'openweather-mqtt-service')
+MQTT_SERVICE_USER_1 = os.getenv('MQTT_SERVICE_USER_1')
+MQTT_SERVICE_USER_2 = os.getenv('MQTT_SERVICE_USER_2')
+MQTT_SERVICE_PW_1 = os.getenv('MQTT_SERVICE_PW_1')
+MQTT_SERVICE_PW_2 = os.getenv('MQTT_SERVICE_PW_2')
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s [%(name)s] %(levelname)8s %(message)s')
-logger = logging.getLogger(MQTT_CLIENT_ID)
+logger = logging.getLogger(MQTT_CLIENT_ID_1)
+logger = logging.getLogger(MQTT_CLIENT_ID_2)
 
 # Display config on startup
 logger.debug("#" * 80)
 logger.debug(f"# OPENWEATHER_APP_ID={OPENWEATHER_APP_ID}")
-logger.debug(f"# OPENWEATHER_CITY_ID={OPENWEATHER_CITY_ID}")
-logger.debug(f"# MQTT_SERVICE_HOST={MQTT_SERVICE_HOST}")
-logger.debug(f"# MQTT_SERVICE_PORT={MQTT_SERVICE_PORT}")
-logger.debug(f"# MQTT_SERVICE_TOPIC={MQTT_SERVICE_TOPIC}")
-logger.debug(f"# MQTT_CLIENT_ID={MQTT_CLIENT_ID}")
-logger.debug(f"# MQTT_SERVICE_USER={MQTT_SERVICE_USER}")
-logger.debug(f"# MQTT_SERVICE_PW={MQTT_SERVICE_PW}")
+logger.debug(f"# OPENWEATHER_CITY_ID_1={OPENWEATHER_CITY_ID_1}")
+logger.debug(f"# MQTT_SERVICE_HOST_1={MQTT_SERVICE_HOST_1}")
+logger.debug(f"# MQTT_SERVICE_PORT_1={MQTT_SERVICE_PORT_1}")
+logger.debug(f"# MQTT_SERVICE_TOPIC_1={MQTT_SERVICE_TOPIC_1}")
+logger.debug(f"# MQTT_CLIENT_ID_1={MQTT_CLIENT_ID_1}")
+logger.debug(f"# MQTT_SERVICE_USER_1={MQTT_SERVICE_USER_1}")
+logger.debug(f"# MQTT_SERVICE_PW_1={MQTT_SERVICE_PW_1}")
+logger.debug(f"# -------------------------------------------")
+logger.debug(f"# OPENWEATHER_CITY_ID_2={OPENWEATHER_CITY_ID_2}")
+logger.debug(f"# MQTT_SERVICE_HOST_2={MQTT_SERVICE_HOST_2}")
+logger.debug(f"# MQTT_SERVICE_PORT_2={MQTT_SERVICE_PORT_2}")
+logger.debug(f"# MQTT_SERVICE_TOPIC_2={MQTT_SERVICE_TOPIC_2}")
+logger.debug(f"# MQTT_CLIENT_ID_2={MQTT_CLIENT_ID_2}")
+logger.debug(f"# MQTT_SERVICE_USER_2={MQTT_SERVICE_USER_2}")
+logger.debug(f"# MQTT_SERVICE_PW_2={MQTT_SERVICE_PW_2}")
 logger.debug("#" * 80)
 
 
@@ -70,7 +86,7 @@ if __name__ == "__main__":
         try:
 
             logger.info("Connecting to OpenWeather for fresh weather information.")
-            url = f"http://api.openweathermap.org/data/2.5/weather?id={OPENWEATHER_CITY_ID}&appid={OPENWEATHER_APP_ID}&type=accurate&units=metric&lang=de"
+            url = f"http://api.openweathermap.org/data/2.5/weather?id={OPENWEATHER_CITY_ID_1}&appid={OPENWEATHER_APP_ID}&type=accurate&units=metric&lang=de"
             r = requests.get(url)
             data = r.json()
 
@@ -82,10 +98,15 @@ if __name__ == "__main__":
             if int(data['dt']) >= int(previous_last_update):
                 previous_last_update = int(data['dt'])
 
-                msgs = []
+                msgs1 = []
                 for k, v in sorted(flatten_dict(data, delimiter='/').items()):
                     logger.info(f"{k:24} ---> {v}")
-                    msgs.append({'topic': f"{MQTT_SERVICE_TOPIC}/{k}", 'payload': str(v)})
+                    msgs1.append({'topic': f"{MQTT_SERVICE_TOPIC_1}/{k}", 'payload': str(v)})
+
+                msgs2 = []
+                for k, v in sorted(flatten_dict(data, delimiter='/').items()):
+                    logger.info(f"{k:24} ---> {v}")
+                    msgs2.append({'topic': f"{MQTT_SERVICE_TOPIC_2}/{k}", 'payload': str(v)})
             else:
                 logger.info("No updated data from Openweather...")
 
@@ -93,20 +114,30 @@ if __name__ == "__main__":
             # but call Openweather API every ~1min (otherwise you'll get locked due to API rate limits)
             last_update = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(data['dt']))
             for i in range(60):
-                logger.info(f"Publishing to {MQTT_SERVICE_HOST}:{MQTT_SERVICE_PORT} [last_update={last_update}]")
+                logger.info(f"Publishing to {MQTT_SERVICE_HOST_1}:{MQTT_SERVICE_PORT_1} [last_update={last_update}]")
+                logger.info(f"Publishing to {MQTT_SERVICE_HOST_2}:{MQTT_SERVICE_PORT_2} [last_update={last_update}]")
                 # Building the parameters for the publish.multiple() call
-                kwargs = {
-                    'hostname': MQTT_SERVICE_HOST,
-                    'port': MQTT_SERVICE_PORT,
-                    'client_id': MQTT_CLIENT_ID
+                kwargs1 = {
+                    'hostname': MQTT_SERVICE_HOST_1,
+                    'port': MQTT_SERVICE_PORT_1,
+                    'client_id': MQTT_CLIENT_ID_1
+                }
+
+                kwargs2 = {
+                    'hostname': MQTT_SERVICE_HOST_2,
+                    'port': MQTT_SERVICE_PORT_2,
+                    'client_id': MQTT_CLIENT_ID_2
                 }
 
                 # Only add auth if user and password are available
-                if MQTT_SERVICE_USER and MQTT_SERVICE_PW:
-                    kwargs['auth'] = {'username': MQTT_SERVICE_USER, 'password': MQTT_SERVICE_PW}
+                if MQTT_SERVICE_USER_1 and MQTT_SERVICE_PW_1:
+                    kwargs1['auth'] = {'username': MQTT_SERVICE_USER_1, 'password': MQTT_SERVICE_PW_1}
+                if MQTT_SERVICE_USER_2 and MQTT_SERVICE_PW_2:
+                    kwargs1['auth'] = {'username': MQTT_SERVICE_USER_2, 'password': MQTT_SERVICE_PW_2}
 
                 # Call publish.multiple with the appropriate parameters
-                publish.multiple(msgs, **kwargs)
+                publish.multiple(msgs1, **kwargs1)
+                publish.multiple(msgs2, **kwargs2)
 
                 time.sleep(1)
 
